703b20c4d7ee097f79b03cefb01a70da08ae2781
Katarzyna Jakubiec <kate_1995@tlen.pl>
2022-03-30 14:17:46 +0200

    border fixed

--- first/first.js
+++ first/first.js
@@ -8,39 +8,39 @@ fetch('http://rekrutacjartb.pl/developer/banner.json')
                     <img class="image" src=http:${result.offers[i].imgURL} >
                     <p class="price">${result.offers[i].price} ${result.offers[i].currency}</p>
                 </div>
-            `;
-        document.querySelector('.offers').innerHTML = offers;
-    
-        let counter = 0
+             `;
+         document.querySelector('.offers').innerHTML = offers;
 
-        setTimeout(() => {
-            if (counter < 4) {
-                document.getElementById("product"+counter).style.borderColor = "a8a8a8";
-                counter++;
-            }
-            else {
-                secondcounter = 0;
-            }
-            setInterval(function() {
-            if (counter < 4) {
-                document.getElementById("product"+counter).style.borderColor = "a8a8a8";
-                counter++;
-            }
-            else {
-                secondcounter = 0;
-            }
-            }, 2000)
-        }, 4000)
+         let counter = 0
 
-        let secondcounter = 0;
+         setTimeout(function() {
+             if (counter < 4) {
+                 document.getElementById("product"+counter).style.borderColor = "a8a8a8";
+                 counter++;
+             }
+             else {
+                 secondcounter = 0;
+             }
+             setInterval(function() {
+             if (counter < 4) {
+                 document.getElementById("product"+counter).style.borderColor = "a8a8a8";
+                 counter++;
+             }
+             else {
+                 secondcounter = 0;
+             }
+             }, 2000)
+         }, 4000)
 
-        setInterval(function(){ 
-            if (secondcounter < 4) {
-                document.getElementById("product"+secondcounter).style.borderColor = "ffff00";
-                secondcounter++;
-            } else {
-                secondcounter = 0;
-            }
+         let secondcounter = 0;
+
+         setInterval(function(){ 
+             if (secondcounter < 4) {
+                 document.getElementById("product"+secondcounter).style.borderColor = "ffff00";
+                 secondcounter++;
+             } else {
+                 secondcounter = 0;
+             }
         }, 2000);
 
     }
